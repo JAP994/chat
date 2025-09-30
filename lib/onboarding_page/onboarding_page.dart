ea6b137721a098fdd0c086d0afc167546bc49983
JAP994 <jonathansanchezponce@MacBook-Air-de-Jonathan.local>
2025-09-30 14:38:16 -0500

    Inicio del proyecto chat

---
+++ lib/onboarding_page/onboarding_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('assets/icons/icon_app.png')],
+        ),
+      ),
+    );
+  }
+}
