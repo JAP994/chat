671fd23f0372efdf569e4eb8a4c2168245059c0a
JAP994 <jonathansanchezponce@MacBook-Air-de-Jonathan.local>
2025-10-01 14:48:28 -0500

    correciones opacity

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -23,7 +23,9 @@ class LoginPage extends StatelessWidget {
               style: textTheme.labelLarge?.copyWith(
                 decoration: TextDecoration.underline,
                 decorationThickness: 10,
-                decorationColor: theme.colorScheme.primary.withOpacity(0.7),
+                decorationColor: theme.colorScheme.primary.withValues(
+                  alpha: 0.7,
+                ),
               ),
               children: [
                 TextSpan(text: ' to Chateo', style: textTheme.labelLarge),
